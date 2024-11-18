e05ae7a1235ec6e35eb1a5afdf29d78bb6d577dc
Mounish <mounishch23@gmail.com>
2024-11-18 09:26:06 +0530

    updated

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">MOUNISH</div>
+          <div className="right">
+            <p>BATCH B, TEAM 11</p>
+            <p>WE, THE TEAM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By MOUNISH</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By TEAM11.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
